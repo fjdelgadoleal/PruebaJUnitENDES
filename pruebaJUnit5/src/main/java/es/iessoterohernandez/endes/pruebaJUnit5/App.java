ccb831706b6d71457a4fd4ae19374a5a3dea4cee
Javi <fjdelgadoleal@yahoo.es>
2021-02-16 11:15:14 +0100

    New commit

--- pruebaJUnit5/src/main/java/es/iessoterohernandez/endes/pruebaJUnit5/App.java
+++ pruebaJUnit5/src/main/java/es/iessoterohernandez/endes/pruebaJUnit5/App.java
@@ -3,7 +3,7 @@ package es.iessoterohernandez.endes.pruebaJUnit5;
 public class App {
     
     public static void main(String[] args) {
-    	System.out.println("Hola chicos!");
+    	System.out.println("Hola chicos de primero!");
     }
     
 }
